e6d54a1e6ec457c150d38be1f84f8b9975136f76
Guilherme Gouveia Figueiredo Lima <ggfl@cin.ufpe.br>
2018-12-03 23:23:18 -0300

    trails bonitos porém lentos pra porra

--- graphics/pong/Main.hs
+++ graphics/pong/Main.hs
@@ -1,6 +1,5 @@
 module Main(main) where
 import Graphics.Gloss
-import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 
 -- The game setup
@@ -20,9 +19,9 @@ initialState = Game
     {
         player1 = (-10,30)
     ,   player2 = (0, -3)
-    ,   p1xVel = 10
+    ,   p1xVel = speed
     ,   p1yVel = 0
-    ,   p2yVel = 10
+    ,   p2yVel = speed
     ,   p2xVel = 0
     ,   p1Trace = []
     ,   p2Trace = []
@@ -32,22 +31,22 @@ initialState = Game
 render :: TronGame -> Picture
 render game = pictures [
                         limits,
-                        paddles,
-                        traces
+                        traces,
+                        paddles
                         ]
     where
-        paddles = pictures [mkPaddle rose (fst (player1 game))(snd(player1 game)),mkPaddle orange (fst (player2 game))(snd(player2 game))]
+        paddles = pictures [mkPaddle blue (fst (player1 game))(snd(player1 game)),mkPaddle red (fst (player2 game))(snd(player2 game))]
         limits = pictures [rotate 90 $ wall (greyN 0.5) 0 150, rotate 90 $ wall (greyN 0.5) 0 (-150), wall (greyN 0.5) 0 150, wall (greyN 0.5) 0 (-150)]
-        traces = pictures [mkTrace rose (p1Trace game), mkTrace orange (p2Trace game)]
+        traces = pictures [mkTrace (light (light blue)) (p1Trace game), mkTrace (light (light red)) (p2Trace game)]
    
 -- Some parameters
-paddleColor = light (light blue)
 background = white
 fps = 60
 limits = [(0, 150), (0, (-150)), (150, 0), ((-150), 0)]
+speed = 30
 
 playerSize :: (Float, Float)
-playerSize = (26, 86)
+playerSize = (10, 10)
     
 -- Factories
 wall :: Color -> Float -> Float -> Picture
@@ -55,18 +54,16 @@ wall wallColor x y = translate x y $ color wallColor $ rectangleSolid 270 10
 
 mkPaddle :: Color -> Float -> Float -> Picture
 mkPaddle col x y = pictures
-    [ translate x y $ color col $ rectangleSolid (fst playerSize) (snd playerSize)
-    , translate x y $ color paddleColor $ rectangleSolid 20 80
-    ]
+    [ translate x y $ color col $ rectangleSolid (fst playerSize) (snd playerSize)]
 
 mkTrace :: Color -> [(Float, Float)] -> Picture
 mkTrace _ [] = pictures []
-mkTrace col n@((x,y):z) = pictures [translate x y $ color col $ rectangleSolid 26 86, mkTrace col z]
+mkTrace col n@((x,y):z) = pictures [translate x y $ color col $ rectangleSolid (fst playerSize) (snd playerSize) , mkTrace col z]
 
 
 -- User Input
 handleKeys :: Event -> TronGame -> TronGame
-handleKeys (EventKey (Char 's') _ _ _) game = Game {
+handleKeys (EventKey (Char 'r') _ _ _) game = Game {
         player1 = (-10,30)
     ,   player2 = (0, -3)
     ,   p1xVel = 10
@@ -75,6 +72,86 @@ handleKeys (EventKey (Char 's') _ _ _) game = Game {
     ,   p2xVel = 0
     ,   p1Trace = []
     ,   p2Trace = []
+    }
+handleKeys (EventKey (SpecialKey KeyUp) _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = 0
+    ,   p1yVel = speed
+    ,   p2yVel = p2yVel game
+    ,   p2xVel = p2xVel game
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
+    }
+handleKeys (EventKey (SpecialKey KeyDown) _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = 0
+    ,   p1yVel = -speed
+    ,   p2yVel = p2yVel game
+    ,   p2xVel = p2xVel game
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
+    }
+handleKeys (EventKey (SpecialKey KeyRight) _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = speed
+    ,   p1yVel = 0
+    ,   p2yVel = p2yVel game
+    ,   p2xVel = p2xVel game
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
+    }
+handleKeys (EventKey (SpecialKey KeyLeft) _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = -speed
+    ,   p1yVel = 0
+    ,   p2yVel = p2yVel game
+    ,   p2xVel = p2xVel game
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
+    }
+handleKeys (EventKey (Char 'w') _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = p1xVel game
+    ,   p1yVel = p1yVel game
+    ,   p2yVel = speed
+    ,   p2xVel = 0
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
+    }
+handleKeys (EventKey (Char 's') _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = p1xVel game
+    ,   p1yVel = p1yVel game
+    ,   p2yVel = -speed
+    ,   p2xVel = 0
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
+    }
+handleKeys (EventKey (Char 'a') _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = p1xVel game
+    ,   p1yVel = p1yVel game
+    ,   p2yVel = 0
+    ,   p2xVel = -speed
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
+    }
+handleKeys (EventKey (Char 'd') _ _ _) game = Game {
+        player1 = player1 game
+    ,   player2 = player2 game
+    ,   p1xVel = p1xVel game
+    ,   p1yVel = p1yVel game
+    ,   p2yVel = 0
+    ,   p2xVel = speed
+    ,   p1Trace = p1Trace game
+    ,   p2Trace = p2Trace game
     }
 handleKeys _ game = game
 
@@ -114,16 +191,17 @@ teleport game = game { player1 = np1, player2 = np2 }
     where
         n@(p1x, p1y) = player1 game
         m@(p2x, p2y) = player2 game   
-        np1 = if wallCollision n (26, 86)
+        np1 = if wallCollision n playerSize
               then
                     (-10, 30)
                else
                     (n)
-        np2 = if wallCollision m (26, 86)
+        np2 = if wallCollision m playerSize
                 then
                     (0, -3)
                 else
                     (m)
+
 
 
 ------------------------------------------------------------
