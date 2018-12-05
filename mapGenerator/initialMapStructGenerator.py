0bec81059881dfaf8825ceebc4dfdad089cea55d
ClaudioCarvalhoo <cco2@cin.ufpe.br>
2018-12-05 11:28:39 -0200

    Create initialMapStructGenerator.py

---
+++ mapGenerator/initialMapStructGenerator.py
@@ -0,0 +1,16 @@
+size = 100
+map = "["
+
+for i in range(size):
+    map+=("[")
+    for j in range(size):
+        if i==0 or j==0 or i==size-1 or j==size-1:
+            map+=("Wall, ")
+        else:
+            map+=("Nada, ")
+    map = map[:-2]
+    map+=("],")
+    map+=("\n")
+map = map[:-2]
+map+=("]")
+print(map)
