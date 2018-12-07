750c02a6010e7150f4efaa914df648e6b6d9fe06
Gui-Lima <ggfl@cin.ufpe.br>
2018-12-06 21:04:34 -0300

    Variables and data used across all files

---
+++ the game/src/Data.hs
@@ -0,0 +1,74 @@
+module Data where
+    import Types
+    import TronGame
+    import Map
+
+    initialPointMap1Player1 :: Position
+    initialPointMap1Player1 = (1,1)
+
+    initialPointMap1Player2 :: Position
+    initialPointMap1Player2 = (1,68)
+
+    initialPointMap2Player1 :: Position
+    initialPointMap2Player1 = (1,1)
+
+    initialPointMap2Player2 :: Position
+    initialPointMap2Player2 = (68, 68)
+
+    initialPointMap3Player1 :: Position
+    initialPointMap3Player1 = (20, 20)
+
+    initialPointMap3Player2 :: Position
+    initialPointMap3Player2 = (60, 60)
+
+    initialSpeed :: Int
+    initialSpeed = 1
+
+
+    initialState :: TronGame
+    initialState = Game
+        {
+            tronMap = menuMap
+        ,   player1 = initialPointMap1Player1
+        ,   player2 = initialPointMap1Player2
+        ,   p1xVel =  initialSpeed
+        ,   p1yVel = 0
+        ,   p2xVel = initialSpeed
+        ,   p2yVel = 0
+        ,   p1Trace = [initialPointMap1Player1]
+        ,   p2Trace = [initialPointMap1Player2]
+        ,   p1dead = False
+        ,   p2dead = False
+        }
+
+    map1State :: TronGame
+    map1State = Game
+        {
+            tronMap = simpleMap
+        ,   player1 = initialPointMap2Player1
+        ,   player2 = initialPointMap2Player2
+        ,   p1xVel =  initialSpeed
+        ,   p1yVel = 0
+        ,   p2xVel = -initialSpeed
+        ,   p2yVel = 0
+        ,   p1Trace = [initialPointMap1Player1]
+        ,   p2Trace = [initialPointMap1Player2]
+        ,   p1dead = False
+        ,   p2dead = False
+        }
+
+    map2State :: TronGame
+    map2State = Game
+        {
+            tronMap = obstacleMap
+        ,   player1 = initialPointMap3Player1
+        ,   player2 = initialPointMap3Player2
+        ,   p1xVel =  initialSpeed
+        ,   p1yVel = 0
+        ,   p2xVel = -initialSpeed
+        ,   p2yVel = 0
+        ,   p1Trace = [initialPointMap1Player1]
+        ,   p2Trace = [initialPointMap1Player2]
+        ,   p1dead = False
+        ,   p2dead = False
+        } 
